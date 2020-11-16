debf1af509986f7225b89d63e171457139390cce
isabe <isabe@DESKTOP-886E80I.lan>
2020-11-16 16:50:27 +0400

    update

--- SavingFiles.s
+++ SavingFiles.s
@@ -1,8 +1,8 @@
-;how to make new files:
+;How to make new files:
     ; right click on 'Source Files' and click 'New' > 'AssemblyFile.S' 
     ;name file appropriately
     
-;how to save new files:
+;How to save new files:
     ; FIRST you must right click on the file and select 'Git' > 'Commit' 
     ; THEN you can click 'Push upstream' 
     ; if this still doesn't show in your GitHub, then click 'Push' and go through the steps
@@ -10,4 +10,8 @@
 ;In order to save any changes you must always do Git Commit FIRST then Push
     ; Alternatively, you can click the Team tab and click Commit there, and then Push
 
+;Note that if the file is 'green' it is because you have note yet committed/saved changes
 
+;Note that if you change file names on MPLab, this saves to GitHub as a new file:
+	;therefore you must go to GitHub and delete the file with the old file name manually
+	;this applies to any files you want deleted, deleting them on MPLab will NOT delete them on GitHub
