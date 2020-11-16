5398b7454865c11a23c27fac7e45a873328b43e5
isabe <isabe@DESKTOP-886E80I.lan>
2020-11-16 16:46:07 +0400

    update

--- noconfig.s
+++ noconfig.s
@@ -8,6 +8,6 @@
     ; if this still doesn't show in your GitHub, then click 'Push' and go through the steps
     
  ; In order to save any changes you must always do Git Commit FIRST then Push
-    ; Alternatively, you can click the Team tab and click Commit there
+    ; Alternatively, you can click the Team tab and click Commit there, and then Push
 
 
