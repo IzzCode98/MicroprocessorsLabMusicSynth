7e716814da5230b2e87be6a234847e0b68fb96f9
isabe <isabe@DESKTOP-886E80I>
2020-11-20 12:50:57 +0400

    Combing keypad and DAC code

--- Keypad_Input.s
+++ Keypad_Input.s
@@ -1,26 +1,22 @@
 #include <pic18_chip_select.inc>
 #include <xc.inc>
 
-
+global	Keypad_Setup, Keypad_Loading
 psect code, abs
 
-;
-;main:
-;    org 0x0
-;    goto    keypad_setup
-;    org     0x100     ; Main code starts here at address 0x100
+;uses PORT D and PORT E
  
-keypad_setup:   
-    clrf    TRISH, A     ; sets PORTH as output
+Keypad_Setup:   
+    clrf    TRISD, A     ; sets PORTD as output
     banksel PADCFG1     ; selects bank to the location of PADCFG1
     bsf     REPU     ; PORT e PULLUPS on
 
     movlb   0x00     ; set bsr back to Bank 0
 
     clrf    LATE, A     ; sets PORTE as output
-    bra	    keypad_loading
+    return
 
-keypad_loading:
+Keypad_Loading:
     movlw   0x0f     ; 00001111 binary for first four bits as input
     movwf   TRISE, A     ; set tristate D value to be 1's for 1st four pins therefore input pins
 
@@ -40,15 +36,16 @@ keypad_loading:
     movff   PORTE, 0x40    
 
     movlw   0x0
-    bra     loaddata
+    call     loaddata
+    return
 
 loaddata:
     movf    0x30, W, A
     iorwf   0x40, W, A
     movwf   0x50, A
-    movff   0x50, PORTH, A
+    movff   0x50, PORTD, A
 
-    goto    keypad_loading     ; Re-run program from start
+    return     ; Loops in main file
 
 delay:
 
@@ -58,3 +55,5 @@ delay:
 
 
 end
+
+
